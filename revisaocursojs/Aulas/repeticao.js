b34c0a301d750b65476dfa4989c92982fdcbd1cf
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-04-12 18:48:43 -0300

    Create repeticao.js

---
+++ revisaocursojs/Aulas/repeticao.js
@@ -0,0 +1,21 @@
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+ var c = 1
+//estrutura de repetiçao com teste no inicio
+ while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+ }
+// estrutura de repetiçao com teste no final
+ do{
+    console.log(`Passo ${c}`)
+ } while (c <= 6)
+
+ // for (inicio; condição; incremento){}
+
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
