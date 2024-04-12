70ef7108d7419335b4763229bae0f530837d9cf5
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-04-12 16:45:48 -0300

    Exercício do verificador de idade concluído

--- revisaocursojs/Exercícios js/Ex Verificador de Idade/script.js
+++ revisaocursojs/Exercícios js/Ex Verificador de Idade/script.js
@@ -1,6 +1,44 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.querySelector('div#res');
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[erro]');
+    } else {
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+
+        if (fsex[0].checked) {
+            genero = 'Homem';
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'homem_criança.jpg');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'homem_adulto.jpg');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'homem_adulto.jpg');
+            } else {
+                img.setAttribute('src', 'homem_velho.jpg');
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher';
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'mulher_criança.jpg');
+            } else if (idade < 21) {
+                img.setAttribute('src', 'mulher_adolecente.jpg');
+            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher_adulta.jpg');
+            } else {
+                img.setAttribute('src', 'mulher_idosa.jpg');
+            }
+        }
+        
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+        res.appendChild(img);
+    }
 }
