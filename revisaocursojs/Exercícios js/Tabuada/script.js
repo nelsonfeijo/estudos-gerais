bd24c8429208608b32c72e96da35cff24a432e66
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-04-13 11:49:24 -0300

    Exercício gerador de tabuada

---
+++ revisaocursojs/Exercícios js/Tabuada/script.js
@@ -0,0 +1,14 @@
+function tabuada() {
+    var numero = parseInt(document.getElementById('num').value);
+    var res = document.getElementById('res');
+    res.innerHTML = '';
+    if (numero == 0) {
+        res.innerHTML = 'A resposta de todos os valores é 0';
+    }else {
+        for (var c = 0; c <= 10; c += 1) {
+            var nume = numero*c
+            res.innerHTML += `${numero} X ${c} = ${nume} <br>`;
+    }
+    }
+    res.innerHTML += '\u{1F3C1}'
+}
